4a8987d853491b04bfef531cfe7f79af07852f24
Marc Scott <marc@raspberrypi.org>
2017-01-17 10:00:14 +0000

    Code changes

--- code/interface.py
+++ code/interface.py
@@ -40,26 +40,29 @@ for msg in listen():
     if 'id' in msg['sensor-update']:
         id = msg['sensor-update']['id']
         url = 'https://apex.oracle.com/pls/apex/raspberrypi/weatherstation/getlatestwsdata/' + id
-        weather = get(url).json()['items'][0]
         try:
-            town, country = getplace(weather['weather_stn_lat'],weather['weather_stn_long'])
+            weather = get(url).json()['items'][0]
+            try:
+                town, country = getplace(weather['weather_stn_lat'],weather['weather_stn_long'])
+            except:
+                town = 'Unknown'
+                country = 'Unknown'
+            s.sensorupdate({'air_pressure':weather['air_pressure']})
+            s.sensorupdate({'wind_gust_speed':weather['wind_gust_speed']})
+            s.sensorupdate({'wind_direction':weather['wind_direction']})
+            s.sensorupdate({'reading_timestamp':weather['reading_timestamp']})
+            s.sensorupdate({'wind_speed':weather['wind_speed']})
+            s.sensorupdate({'air_quality':weather['air_quality']})
+            s.sensorupdate({'ground_temp':weather['ground_temp']})
+            s.sensorupdate({'humidity':weather['humidity']})
+            s.sensorupdate({'weather_stn_lat':weather['weather_stn_lat']})
+            s.sensorupdate({'weather_stn_long':weather['weather_stn_long']})
+            s.sensorupdate({'ambient_temp':weather['ambient_temp']})
+            s.sensorupdate({'rainfall':weather['rainfall']})
+            s.sensorupdate({'town':town.replace(' ','-')})
+            s.sensorupdate({'country':country.replace(' ','-')})
         except:
-            town = 'Unknown'
-            country = 'Unknown'
-        s.sensorupdate({'air_pressure':weather['air_pressure']})
-        s.sensorupdate({'wind_gust_speed':weather['wind_gust_speed']})
-        s.sensorupdate({'wind_direction':weather['wind_direction']})
-        s.sensorupdate({'reading_timestamp':weather['reading_timestamp']})
-        s.sensorupdate({'wind_speed':weather['wind_speed']})
-        s.sensorupdate({'air_quality':weather['air_quality']})
-        s.sensorupdate({'ground_temp':weather['ground_temp']})
-        s.sensorupdate({'humidity':weather['humidity']})
-        s.sensorupdate({'weather_stn_lat':weather['weather_stn_lat']})
-        s.sensorupdate({'weather_stn_long':weather['weather_stn_long']})
-        s.sensorupdate({'ambient_temp':weather['ambient_temp']})
-        s.sensorupdate({'rainfall':weather['rainfall']})
-        s.sensorupdate({'town':town.replace(' ','-')})
-        s.sensorupdate({'country':country.replace(' ','-')})
+            print('Station data not found. Please choose another ID')
     if 'new-station' in msg['broadcast']:
         station_id = choice(station_list)['weather_stn_id']
  #       print(station_id)
